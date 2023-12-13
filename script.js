dacb5a6bd0588ae1ce33398dcb28347fb3061457
Oleksandr Shkurkin <saha.sanek.555@gmail.com>
2023-12-13 21:31:43 +0100

    initialization

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+$("#ball").on('click', function () {
+    window.open("https://aleksandrshkurkin.github.io/AS.github.io/", "_blank");
+});
+
+$("#num").on('click', function () {
+    window.open("https://aleksandrshkurkin.github.io/blackjack.github.io/", "_blank");
+});
+
+$("#blackjack").on('click', function () {
+    window.open("https://aleksandrshkurkin.github.io/blackjackImproved.github.io/", "_blank");
+});
+
+$("#casino").on('click', function () {
+    window.open("https://aleksandrshkurkin.github.io/casino.github.io/", "_blank");
+});
